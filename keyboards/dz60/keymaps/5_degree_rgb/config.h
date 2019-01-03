647ac0489f3649dd2bb3258e38bc1cf746739fa9
Jin Kim <jinhkim716@gmail.com>
2019-01-02 21:26:51 -0800

    new keyboard: 5 degree RGB
    - new keymap for 5 degree RGB keyboard

---
+++ keyboards/dz60/keymaps/5_degree_rgb/config.h
@@ -0,0 +1,10 @@
+
+#ifdef PRODUCT
+#undef PRODUCT
+#define PRODUCT         Five Degrees Of RGB
+#endif
+
+#ifdef DESCRIPTION
+#undef DESCRIPTION
+#define DESCRIPTION     It has rainbow colors
+#endif
