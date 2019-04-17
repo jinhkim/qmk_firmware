b3c3798be48b6bc0b0b281e056fa8311256df3c9
Jin Kim <jinhkim716@gmail.com>
2019-04-17 00:02:28 -0700

    Add 1976 keyboard
    - currently this is a DZ60 with KBDFans T1 switches

---
+++ keyboards/dz60/keymaps/1976/config.h
@@ -0,0 +1,10 @@
+
+#ifdef PRODUCT
+#undef PRODUCT
+#define PRODUCT         The Keyboard from 1976
+#endif
+
+#ifdef DESCRIPTION
+#undef DESCRIPTION
+#define DESCRIPTION     That groovy keyboard you are typing on
+#endif
