ad5507a03b1dafe2022c06be438733dc05cc49c5
Jin Kim <jinhkim716@gmail.com>
2019-05-03 00:34:33 -0700

    add golden keyboard
    - it's actually made of bronze and brass, but it looks like it's worth
    more than silver

---
+++ keyboards/dz60/keymaps/golden/config.h
@@ -0,0 +1,10 @@
+
+#ifdef PRODUCT
+#undef PRODUCT
+#define PRODUCT         The Golden Keyboard
+#endif
+
+#ifdef DESCRIPTION
+#undef DESCRIPTION
+#define DESCRIPTION     That golden keyboard you are typing on
+#endif
